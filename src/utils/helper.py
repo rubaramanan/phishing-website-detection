4949d11da726f0c70c397bd7930033dde001199c
Ramanan <rubaramanan1997@gmail.com>
2023-11-07 00:46:54 -0500

    update helper functions

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,15 +1,6 @@
-import re
 from urllib.parse import urlparse
-import requests
-import urllib.request
-from bs4 import BeautifulSoup
-from urllib.error import HTTPError
-import whois
-from datetime import datetime
 
-from src.utils.feature_extraction import has_ip, has_at, has_redirection, has_hypen, shortining_service, \
-    has_non_standard_ports, has_suffix_prefix, has_subdomains, is_https, url_length_long, is_active, web_traffic, \
-    has_dns_record, is_sixmonth_old_dns
+import whois
 
 
 def get_scheme(url):
@@ -38,12 +29,3 @@ def get_domain_details(url):
     except Exception as e:
         print(e)
         return None
-
-
-def gen_features(df):
-    funcs = [has_ip, has_at, has_hypen, has_redirection, has_non_standard_ports, has_subdomains, has_suffix_prefix,
-             is_https, url_length_long, shortining_service,
-             is_active, web_traffic, has_dns_record, is_sixmonth_old_dns]
-    for func in funcs:
-        df[func.__name__] = df.url.apply(func)
-    return df
