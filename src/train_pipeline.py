8235502e078bd04af19eb32fc069187986095957
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 15:14:33 -0500

    train pipeline

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -1,17 +1,9 @@
-from src.phish_detector.feedforward import PhishDetectorFeedForward
+from src.phish_detector.train.state_of_the_art import sota_train
 from src.utils.get_features_labels import train_data, validate_data
-from datetime import datetime
 
 dir = '../final_data'
 features, labels = train_data(f"{dir}/Phishing_train_dataset.csv")
 test_features, test_labels = validate_data(f"{dir}/Phishing_test_dataset.csv")
+print(features.shape[1])
 
-
-feedforward = PhishDetectorFeedForward()
-feedforward.train(features,
-                  labels,
-                  X_val=test_features,
-                  y_val=test_labels,
-                  batch_size=32,
-                  epochs=100)
-feedforward.save_model(f"./models/feedforward/feedforwad_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.h5")
+sota_train(features, labels, test_features, test_labels, 100, 32)
