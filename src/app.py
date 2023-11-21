512e59a898a9a28a04c6256215e08eaa88fe5ae5
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 15:14:12 -0500

    flask app file

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask.json import jsonify
+from src.predict_pipeline import predict
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['post'])
+def prediction():
+    url = json.loads(request.data)['url']
+    return jsonify(predict(url))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
