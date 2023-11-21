42a1e307bbedc8bfd8b7be54dec84afc2a1032b8
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 15:14:23 -0500

    predict pipeline

--- src/predict_pipeline.py
+++ src/predict_pipeline.py
@@ -1,11 +1,30 @@
+from src.phish_detector.predict.feed_forward import feedforward_prediction
 from src.utils.feature_extraction import gen_features_predict
-from src.phish_detector.feedforward import PhishDetectorFeedForward
+from src.phish_detector.predict.state_of_the_art import sota_prediction
 
-features = gen_features_predict('http://172.17.0.1:8080/hello')
-print(features)
 
-loaded_model = PhishDetectorFeedForward()
-loaded_model.load_models('./models/feedforward')
+def predict(url):
+    features = gen_features_predict(url)
 
-pred = loaded_model.test([features])
-print(pred)
+    feed_pred = feedforward_prediction(features)
+    sota_pred = sota_prediction(features)
+
+    results = {'GAN result': sota_pred,
+               'Feedforward result': feed_pred}
+
+    return {'Final results': get_final_results(results),
+            'results': results}
+
+
+def get_final_results(results: dict):
+    result_values = list(results.values())
+    print(result_values)
+    leg_count = result_values.count('Legitimate')
+
+    # has to change 1 as 2
+    if leg_count > 1:
+        return 'Legitimate'
+    if leg_count < 1:
+        return 'Phish'
+    if leg_count == 1:
+        return 'Suspicious'
