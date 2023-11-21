183c8597e66ae1d97e2586c0838b564832e73c2f
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 15:11:56 -0500

    get features and labels from data

--- src/utils/get_features_labels.py
+++ src/utils/get_features_labels.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 def train_data(path):
     df = pd.read_csv(path)
-    return df.drop('Result', axis=1), df.Result
+    return df.drop('Result', axis=1).values.astype('float32'), df.Result.values.astype('float32')
 
 
 def validate_data(path):
     df = pd.read_csv(path)
-    return df.drop('Result', axis=1), df.Result
+    return df.drop('Result', axis=1).values.astype('float32'), df.Result.values.astype('float32')
