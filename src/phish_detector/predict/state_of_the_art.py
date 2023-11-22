f21950c40e601677a6eaf078690cc3e69efb92c8
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 21:05:47 -0500

    state of the art refactor

--- src/phish_detector/predict/state_of_the_art.py
+++ src/phish_detector/predict/state_of_the_art.py
@@ -1,10 +1,14 @@
 import torch
 
+from src.utils.feature_extraction import gen_features_predict
 
-def sota_prediction(features):
+
+def sota_prediction(url):
+    features = gen_features_predict(url)
+
     model = torch.load("./models/sota/discriminator.pth")
     pred = model(torch.FloatTensor(features))
     predc = pred.argmax().item()
     labels = {0: 'Legitimate',
               1: 'Phish'}
-    return labels[predc]
+    return {"State of the Art": labels[predc]}
