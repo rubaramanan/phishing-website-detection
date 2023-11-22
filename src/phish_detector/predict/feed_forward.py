5042ce34779253d432c210af491ecd832a64ecd3
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 21:05:57 -0500

    feedforward refactor

--- src/phish_detector/predict/feed_forward.py
+++ src/phish_detector/predict/feed_forward.py
@@ -1,8 +1,11 @@
 from src.phish_detector.classes.feedforward import PhishDetectorFeedForward
+from src.utils.feature_extraction import gen_features_predict
 
 
-def feedforward_prediction(features):
+def feedforward_prediction(url):
+    features = gen_features_predict(url)
+
     loaded_model = PhishDetectorFeedForward()
     loaded_model.load_models('./models/feedforward')
     pred = loaded_model.test([features])
-    return pred
+    return {"Neural Network": pred}
