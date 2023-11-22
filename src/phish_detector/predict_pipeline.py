b7ec1aa44ac81082ad857aca51970321672d2391
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 21:04:43 -0500

    predict pipeline refactor

---
+++ src/phish_detector/predict_pipeline.py
@@ -0,0 +1,25 @@
+from src.phish_detector.predict.feed_forward import feedforward_prediction
+from src.phish_detector.predict.state_of_the_art import sota_prediction
+
+
+def predict(url):
+
+    results = feedforward_prediction(url)
+    results |= sota_prediction(url)
+
+    return {'Voted weight': voted_weight_result(results)} | results
+
+
+def voted_weight_result(results: dict):
+    result_values = list(results.values())
+    print(result_values)
+    leg_count = result_values.count('Legitimate')
+
+    threshold = len(result_values) / 2
+
+    if leg_count > threshold:
+        return 'Legitimate'
+    if leg_count < threshold:
+        return 'Phish'
+    if leg_count == threshold:
+        return 'Suspicious'
