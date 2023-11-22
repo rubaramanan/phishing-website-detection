de5831c0411efee90362f31a39eb4ae90d0f8afe
Ramanan <rubaramanan1997@gmail.com>
2023-11-21 21:08:52 -0500

    train pipeline refactor

--- src/phish_detector/train_pipeline.py
+++ src/phish_detector/train_pipeline.py
@@ -1,9 +1,11 @@
 from src.phish_detector.train.state_of_the_art import sota_train
+from src.phish_detector.train.feed_forwad import feedforward_train
 from src.utils.get_features_labels import train_data, validate_data
 
 dir = '../../final_data'
 features, labels = train_data(f"{dir}/Phishing_train_dataset.csv")
 test_features, test_labels = validate_data(f"{dir}/Phishing_test_dataset.csv")
-print(features.shape[1])
 
+feedforward_train(features, labels, test_features, test_labels, 100, 32)
 sota_train(features, labels, test_features, test_labels, 100, 32)
+
