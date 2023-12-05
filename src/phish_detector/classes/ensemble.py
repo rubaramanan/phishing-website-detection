e61ca4a80bf2c8aeec5ac9d77339f5bc05fb648d
Ramanan <rubaramanan1997@gmail.com>
2023-12-05 12:50:07 -0500

    ensemble classes

---
+++ src/phish_detector/classes/ensemble.py
@@ -0,0 +1,31 @@
+import xgboost
+import numpy as np
+from pathlib import Path
+import pickle
+import os
+
+
+class PhishDetectorXGBoost:
+    def __init__(self):
+        self.model = xgboost.XGBClassifier()
+
+    def train(self, X, y, X_val=None, y_val=None):
+        self.model.fit(X, y)
+        score = self.model.score(X_val, y_val)
+        print(f"XGBoost's validation accuracy: {score}")
+
+    def test(self, X):
+        pred = self.model.predict(X)
+        labels = {0: 'Legitimate',
+                  1: 'Phish'}
+        key = np.argmax(pred)
+        return labels[key]
+
+    def save_model(self, path):
+        with open(path, 'wb') as f:
+            pickle.dump(self.model, f)
+
+    def load_models(self, path):
+        paths = sorted(Path(path).iterdir(), key=os.path.getmtime)
+        with open(paths[0], 'rb') as f:
+            self.model = pickle.load(f)
