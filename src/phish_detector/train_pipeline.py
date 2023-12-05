0146599dde538604583ee9aa727061de4d90a106
Ramanan <rubaramanan1997@gmail.com>
2023-12-05 12:52:16 -0500

    train pipeline update

--- src/phish_detector/train_pipeline.py
+++ src/phish_detector/train_pipeline.py
@@ -1,7 +1,11 @@
 from src.phish_detector.train.feed_forwad import feedforward_train
 from src.phish_detector.train.state_of_the_art import sota_train
+from src.phish_detector.train.ensemble import xgb_train
+from src.phish_detector.train.traditional_ml import lr_train
 
 
 def train(X, y, X_val, y_val, epochs, batch_size):
     feedforward_train(X, y, X_val, y_val, epochs, batch_size)
     sota_train(X, y, X_val, y_val, epochs, batch_size)
+    xgb_train(X, y, X_val, y_val)
+    lr_train(X, y, X_val, y_val)
